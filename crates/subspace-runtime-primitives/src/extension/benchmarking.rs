09b7acf44f58ffc0d2cdc1f54e6386c1de6f81ca
teor <teor@riseup.net>
2025-06-04 12:54:08 +1000

    Use different call types to avoid premature benchmark optimisations

--- crates/subspace-runtime-primitives/src/extension/benchmarking.rs
+++ crates/subspace-runtime-primitives/src/extension/benchmarking.rs
@@ -8,7 +8,7 @@ use frame_benchmarking::v2::*;
 use frame_support::dispatch::{DispatchInfo, PostDispatchInfo};
 use frame_system::Config;
 use frame_system::pallet_prelude::RuntimeCallFor;
-use pallet_balances::{Call as BalancesCall, Config as BalancesConfig};
+use pallet_balances::{AdjustmentDirection, Call as BalancesCall, Config as BalancesConfig};
 use pallet_multisig::{Call as MultisigCall, Config as MultisigConfig};
 use pallet_utility::{Call as UtilityCall, Config as UtilityConfig};
 use scale_info::prelude::boxed::Box;
@@ -25,6 +25,7 @@ const SEED: u32 = 0;
 #[benchmarks(where
 	T: Send + Sync + scale_info::TypeInfo + fmt::Debug +
         UtilityConfig + BalanceTransferChecks + BalancesConfig + MultisigConfig,
+    <T as BalancesConfig>::Balance: From<u128>,
     RuntimeCallFor<T>:
         Dispatchable<Info = DispatchInfo, PostInfo = PostDispatchInfo> +
         From<UtilityCall<T>> + From<BalancesCall<T>> + From<MultisigCall<T>> +
@@ -38,13 +39,13 @@ mod benchmarks {
     #[benchmark]
     fn balance_transfer_check_multiple(c: Linear<0, 1000>) {
         let mut calls = Vec::with_capacity(c as usize + 1);
-        for _i in 0..=c {
+        for i in 0..=c {
             // Non-balance calls are more expensive to check, because we have to read them all.
             // (We can only exit the check loop early if we encounter a balance transfer call.)
-            calls.push(construct_non_balance_call::<T>());
+            calls.push(construct_non_balance_call::<T>(i));
         }
 
-        let call = construct_utility_call_list::<T>(calls);
+        let call = construct_utility_call_list::<T>(calls, c);
 
         #[block]
         {
@@ -54,9 +55,9 @@ mod benchmarks {
 
     #[benchmark]
     fn balance_transfer_check_utility(c: Linear<0, 1000>) {
-        let mut call = construct_balance_call::<T>();
-        for _i in 0..=c {
-            call = construct_utility_call::<T>(call);
+        let mut call = construct_balance_call::<T>(c);
+        for i in 0..=c {
+            call = construct_utility_call::<T>(call, i);
         }
         #[block]
         {
@@ -66,9 +67,9 @@ mod benchmarks {
 
     #[benchmark]
     fn balance_transfer_check_multisig(c: Linear<0, 1000>) {
-        let mut call = construct_balance_call::<T>();
-        for _i in 0..=c {
-            call = construct_multisig_call::<T>(call);
+        let mut call = construct_balance_call::<T>(c);
+        for i in 0..=c {
+            call = construct_multisig_call::<T>(call, i);
         }
         #[block]
         {
@@ -77,60 +78,139 @@ mod benchmarks {
     }
 }
 
-fn construct_balance_call<T: BalancesConfig>() -> RuntimeCallFor<T>
+/// Construct a banned balance transfer call.
+/// The variant argument is used to make the calls different, so benchmarks are more realistic.
+fn construct_balance_call<T: BalancesConfig>(variant: u32) -> RuntimeCallFor<T>
 where
     RuntimeCallFor<T>: From<BalancesCall<T>>,
+    <T as BalancesConfig>::Balance: From<u128>,
 {
-    let recipient: T::AccountId = account("recipient", 0, SEED);
+    let recipient: T::AccountId = account("recipient", variant, SEED);
     let recipient_lookup = T::Lookup::unlookup(recipient.clone());
-    BalancesCall::transfer_all {
-        dest: recipient_lookup,
-        keep_alive: true,
+
+    match variant % 3 {
+        0 => BalancesCall::transfer_allow_death {
+            dest: recipient_lookup,
+            value: (variant as u128 * 1000).into(),
+        },
+        1 => BalancesCall::transfer_keep_alive {
+            dest: recipient_lookup,
+            value: (variant as u128 * 2000).into(),
+        },
+        _ => BalancesCall::transfer_all {
+            dest: recipient_lookup,
+            keep_alive: variant % 2 == 0,
+        },
     }
     .into()
 }
 
-fn construct_non_balance_call<T: BalancesConfig>() -> RuntimeCallFor<T>
+/// Construct an accepted pallet-balances call.
+/// The variant argument is used to make the calls different, so benchmarks are more realistic.
+fn construct_non_balance_call<T: BalancesConfig>(variant: u32) -> RuntimeCallFor<T>
 where
     RuntimeCallFor<T>: From<BalancesCall<T>>,
+    <T as BalancesConfig>::Balance: From<u128>,
 {
-    let recipient: T::AccountId = account("recipient", 0, SEED);
-    BalancesCall::upgrade_accounts {
-        who: vec![recipient],
+    let recipient: T::AccountId = account("recipient", variant, SEED);
+    let recipient_lookup = T::Lookup::unlookup(recipient.clone());
+
+    // We skip some force_* calls, to avoid confusion with actual balance transfers.
+    match variant % 4 {
+        0 => BalancesCall::upgrade_accounts {
+            who: vec![recipient],
+        },
+        1 => BalancesCall::force_unreserve {
+            who: recipient_lookup,
+            amount: (variant as u128 * 3000).into(),
+        },
+        2 => BalancesCall::force_adjust_total_issuance {
+            direction: AdjustmentDirection::Increase,
+            delta: (variant as u128 * 4000).into(),
+        },
+        _ => BalancesCall::burn {
+            value: (variant as u128 * 5000).into(),
+            keep_alive: variant % 2 == 0,
+        },
     }
     .into()
 }
 
-fn construct_utility_call<T: UtilityConfig>(call: RuntimeCallFor<T>) -> RuntimeCallFor<T>
+fn construct_utility_call<T: UtilityConfig>(
+    call: RuntimeCallFor<T>,
+    variant: u32,
+) -> RuntimeCallFor<T>
 where
     RuntimeCallFor<T>: From<UtilityCall<T>>,
 {
-    UtilityCall::batch_all {
-        calls: vec![call.into()],
+    match variant % 5 {
+        0 => UtilityCall::batch {
+            calls: vec![call.into()],
+        },
+        1 => UtilityCall::as_derivative {
+            index: variant as u16,
+            call: Box::new(call.into()),
+        },
+        2 => UtilityCall::batch_all {
+            calls: vec![call.into()],
+        },
+        3 => UtilityCall::force_batch {
+            calls: vec![call.into()],
+        },
+        /* TODO: implement this, perhaps with a variant for `as_origin`?
+        4 => UtilityCall::dispatch_as {
+            as_origin: Box::new(Default::default()),
+            call: Box::new(call.into()),
+        },
+        */
+        _ => UtilityCall::with_weight {
+            call: Box::new(call.into()),
+            weight: Weight::from_parts(variant as u64, 0),
+        },
     }
     .into()
 }
 
-fn construct_utility_call_list<T: UtilityConfig>(calls: Vec<RuntimeCallFor<T>>) -> RuntimeCallFor<T>
+/// Wrap `calls` in a pallet-utility call.
+/// The variant argument is used to make the calls different, so benchmarks are more realistic.
+fn construct_utility_call_list<T: UtilityConfig>(
+    calls: Vec<RuntimeCallFor<T>>,
+    variant: u32,
+) -> RuntimeCallFor<T>
 where
     RuntimeCallFor<T>: From<UtilityCall<T>>,
 {
-    UtilityCall::batch_all {
-        calls: calls.into_iter().map(Into::into).collect(),
+    let calls = calls.into_iter().map(Into::into).collect();
+
+    match variant % 3 {
+        0 => UtilityCall::batch { calls },
+        1 => UtilityCall::batch_all { calls },
+        _ => UtilityCall::force_batch { calls },
     }
     .into()
 }
 
-fn construct_multisig_call<T: MultisigConfig>(call: RuntimeCallFor<T>) -> RuntimeCallFor<T>
+/// Wrap `calls` in a pallet-multisig call.
+/// The variant argument is used to make the calls different, so benchmarks are more realistic.
+fn construct_multisig_call<T: MultisigConfig>(
+    call: RuntimeCallFor<T>,
+    variant: u32,
+) -> RuntimeCallFor<T>
 where
     RuntimeCallFor<T>: From<MultisigCall<T>> + Into<<T as MultisigConfig>::RuntimeCall>,
 {
-    MultisigCall::as_multi {
-        threshold: 0,
-        other_signatories: vec![],
-        maybe_timepoint: None,
-        call: Box::new(call.into()),
-        max_weight: Weight::zero(),
+    match variant % 2 {
+        0 => MultisigCall::as_multi_threshold_1 {
+            other_signatories: vec![],
+            call: Box::new(call.into()),
+        },
+        _ => MultisigCall::as_multi {
+            threshold: variant as u16,
+            other_signatories: vec![],
+            maybe_timepoint: None,
+            call: Box::new(call.into()),
+            max_weight: Weight::from_parts(variant as u64, 0),
+        },
     }
     .into()
 }
